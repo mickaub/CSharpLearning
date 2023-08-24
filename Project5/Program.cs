27654cf2dcc99954a6aa961a3da35fc9493080b7
mickaub <mickaub3@gmail.com>
2023-08-24 17:43:56 +1000

    add: more methods with para

--- Project5/Program.cs
+++ Project5/Program.cs
@@ -289,6 +289,7 @@ void Clear(int[] array)
     }
 }
 */
+/*
 string status = "Healthy";
 
 Console.WriteLine($"Start: {status}");
@@ -301,4 +302,91 @@ void SetHealth (bool isHealthy)
     Console.WriteLine($"Middle: {status}");
 }
 
-//UPTO part 5: Create C# methods with parameters: Unit 5 of 9 Exercise - Methods with optional parameters
+parameters
+*/
+/*
+string[] guestList = {"Rebecca","Nadia","Noor","Jonte"};
+string[] rsvps = new string[10];
+int count = 0;
+
+RSVP("Rebecca");
+RSVP("Nadia",2,"Nuts");
+RSVP(name:"Linh",partySize:2,inviteOnly:false);
+RSVP("Tony",inviteOnly: true, allergies: "Jackfruit");
+RSVP("Noor",4,inviteOnly:false);
+RSVP("Jonte",2,"Stone fruit",false);
+ShowRSVPs();
+
+void RSVP (string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+{
+    if (inviteOnly)
+    {
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
+}
+*/
+string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string internalDomain = "contoso.com";
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    // display internal email addresses
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    // display external email addresses
+}
+
+void EmailAddress(string domain, string[] name)
+{
+    string firstName = name[0];
+    string lastName = name[1];
+    string address = "";
+
+
+    if (domain == "contoso.com")
+    {
+
+    }
+}
+
+//UPTO part 6: Create C# methods with parameters: Unit 6 of 9 Exercise - Complete the challenge to display email addresses 
