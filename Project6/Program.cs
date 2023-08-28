bb78ff2d81b8ae2cc7f5a2250a5d2210a95fc66e
mickaub <mickaub3@gmail.com>
2023-08-28 11:53:56 +1000

    add: vs debug tools part

--- Project6/Program.cs
+++ Project6/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello World!");
+﻿/* 
+This code uses a names array and corresponding methods to display
+greeting messages
+*/
+
+string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+string messageText = "";
+
+foreach (string name in names)
+{
+    if (name == "Sophia")
+        messageText = SophiaMessage();
+    else if (name == "Andrew")
+        messageText = AndrewMessage();
+    else if (name == "AllGreetings")
+    {
+        messageText = SophiaMessage() + "\n\r" + AndrewMessage();
+    }
+
+    Console.WriteLine(messageText + "\n\r");
+}
+
+bool pauseCode = true;
+while (pauseCode == true);
+
+static string SophiaMessage()
+{
+    return "Hello, my name is Sophia.";
+}
+
+static string AndrewMessage()
+{
+    return "Hi, my name is Andrew. Good to meet you.";
+}
+
+//UPTO Implement the Visual Studio Code debugging tools for C# Unit 7 of 12
